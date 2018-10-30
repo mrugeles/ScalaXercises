929e191ef019ba9fde74005be1d058f86f556edb
dbarrios83 <34863185+dbarrios83@users.noreply.github.com>
2018-10-30 11:25:01 -0500

    test commit

--- src/main/scala/net/Recursion.scala
+++ src/main/scala/net/Recursion.scala
@@ -1,7 +1,12 @@
 package net
 
 class Recursion {
-  def sum(n: Int) = ???
+  def sum(n: Int):Int = {
+      if (n == 0) 0
+      else 1 + sum(n - 1)
+  }
+
+
   def prod(n: Int) = ???
 
   def sumTail(n: Int) = ???
