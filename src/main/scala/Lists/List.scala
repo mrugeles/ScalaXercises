9a5289fca2afb53224203fd0cddec8abd0064c18
dbarrios83 <34863185+dbarrios83@users.noreply.github.com>
2018-11-06 11:31:02 -0500

    List exercises implemented

--- src/main/scala/Lists/List.scala
+++ src/main/scala/Lists/List.scala
@@ -8,20 +8,42 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 object List {
 
 
-  def sum(ints: List[Int]): Int = ???
+  def sum(ints: List[Int]): Int = {
+    ints match {
+      case Nil => 0
+      case Cons(head, tail) => head + sum(tail)
+    }
+  }
+
   //Exercise: Calculate the product of the elements of the list
-  def product(ints: List[Int]): Int = ???
+  def product(ints: List[Int]): Int =     ints match {
+    case Nil => 1
+    case Cons(head, tail) => head * product(tail)
+  }
 
   //Exercise: Remove the first element of the list
-  def tail[A](list: List[A]):List[A] = ???
+  def tail[A](list: List[A]):List[A] = {
+    list match {
+      case Nil => Nil
+      case Cons(head, tail) => tail
+    }
+  }
 
-  //Excercise: Remove the given element of the list
-  def drop[A](list: List[A], n:Int): List[A] = ???
+  //Excercise: Remove the first n elements of the list
+  def drop[A](list: List[A], n:Int): List[A] =  list match {
+    case Nil => Nil
+    case Cons(head, tail) => if (n==1) tail else drop(tail, n-1)
+  }
 
   //Exercise: Remove elements while condition applies
-  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(head, tail) => if (f(head)) dropWhile(tail)(f) else tail
+  }
 
-  def setHead[A](list: List[A], n: A):List[A] = ???
+  def setHead[A](list: List[A], n: A):List[A] = {
+    Cons(n, list)
+  }
 
   def apply[A](as: A*): List[A] =
     if(as.isEmpty) Nil
--- src/test/scala/Lists/ListTest.scala
+++ src/test/scala/Lists/ListTest.scala
@@ -11,6 +11,7 @@ class ListTest {
     Assert.assertEquals(10, sum)
   }
 
+  @Test
   def testProd = {
     val list = List(1, 2, 3, 4)
     val sum = List.product(list)
@@ -25,10 +26,18 @@ class ListTest {
   }
 
   @Test
+  def testTail2 = {
+    val list = List[String]("red")
+    val tailResult = List.tail(list)
+    Assert.assertEquals(tailResult, Nil)
+  }
+
+
+  @Test
   def testDrop = {
     val intList = List(1,2,3,4,5,6,7,8,9)
     val dropResult = List.drop(intList, 3)
-    Assert.assertEquals(dropResult, List(4,5,6,7,8,9))
+    Assert.assertEquals(List(4,5,6,7,8,9), dropResult)
   }
 
 
@@ -36,14 +45,14 @@ class ListTest {
   def testDropWhile = {
     val intList = List(1,2,3,4,5,6,7,8,9)
     val dropWhileResult = List.dropWhile(intList)( n => n < 3)
-    Assert.assertEquals(dropWhileResult, List(3, 4,5,6,7,8,9))
+    Assert.assertEquals(List(3,4,5,6,7,8,9), dropWhileResult)
   }
 
   @Test
   def testSetHead = {
     val intList = List(1,2,3,4,5,6,7,8,9)
-    val setHeadResult = List.setHead(intList, 15 )
-    Assert.assertEquals(setHeadResult, List(1,2,3,4,5,6,7,8,9))
+    val setHeadResult = List.setHead(intList, 15)
+    Assert.assertEquals(setHeadResult, List(15,1,2,3,4,5,6,7,8,9))
 
   }
 
